4cad656f0ad6f8046a8f5cc9e50d6c2fc1299287
Renan Gonçalves <renan.saddam@gmail.com>
2012-03-14 16:48:08 +0100

    Minor fix to avoid 'recursive level too deep' error.

--- lib/Cake/Model/Datasource/DboSource.php
+++ lib/Cake/Model/Datasource/DboSource.php
@@ -1555,7 +1555,7 @@ class DboSource extends DataSource {
 					));
 				} else {
 					$join = array(
-						'table' => $linkModel,
+						'table' => $this->fullTableName($linkModel),
 						'alias' => $association,
 						'type' => isset($assocData['type']) ? $assocData['type'] : 'LEFT',
 						'conditions' => trim($this->conditions($conditions, true, false, $model))
